de25b8744de4500a65e3e921707d1dca1ca929f4
cebuka1 <ebusco29@gmail.com>
2024-05-22 13:50:30 -0700

    update

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,5 @@
+-- Creates the user user_0d_1 with all privileges.
+SELECT title, tv_show_genres.genre_id FROM tv_shows
+LEFT JOIN tv_show_genres ON id=tv_show_genres.show_id
+WHERE tv_show_genres.show_id IS NULL
+ORDER BY title, tv_show_genres.genre_id;
---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,6 @@
+-- Creates the user user_0d_1 with all privileges.
+SELECT name FROM tv_genres
+JOIN tv_show_genres ON id=tv_show_genres.genre_id
+JOIN tv_shows ON tv_shows.id=tv_show_genres.show_id
+WHERE tv_shows.title = 'Dexter'
+ORDER BY name;
